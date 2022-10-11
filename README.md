ef9e3e991cb5bb8265b83bcf76fb66d1040aec16
Erwin Stone <erwinstone@pm.me>
2022-10-11 14:51:01 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # mysql-typebox
-Generate Typebox interfaces from MySQL database.
+
+Generate Typebox interfaces from MySQL database
+
+## Installation
+
+Install `mysql-typebox` with npm
+
+```bash
+npm install mysql-typebox --save-dev
+```
+
+## Usage/Examples
+
+Create a file named `mysql-typebox.json` and fill it as follows (adjust to your needs):
+
+```json
+{
+  "host": "127.0.0.1",
+  "port": 3306,
+  "user": "root",
+  "password": "secret",
+  "database": "myapp"
+}
+```
+
+Create User table:
+
+```sql
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profilePicture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+```
+Then run the command:
+
+```bash
+npx mysql-typebox
+```
+
+The above command will create a `User.ts` file with the following contents:
+
+```typescript
+import { Type } from '@sinclair/typebox'
+
+export const User = Type.Object({
+  id: Type.Number(),
+  name: Type.String(),
+  username: Type.String(),
+  password: Type.String(),
+  profilePicture: Type.Optional(Type.Union([Type.String(), Type.Null()])),
+})
+```
+## Config
+
+`mysql-typebox.json`
+```json
+{
+  "host": "127.0.0.1",
+  "port": 3306,
+  "user": "root",
+  "password": "secret",
+  "database": "myapp",
+  "tables": ["User", "Log"],
+  "ignore": ["Log"],
+  "folder": "@typebox",
+  "suffix": "table"
+}
+```
+
+| Option | Description |
+| ------ | ----------- |
+| tables | Filter the tables to include only those specified. |
+| ignore | Filter the tables to exclude those specified. |
+| folder | Specify the output directory. |
+| suffix | Suffix to the name of a generated file. (eg: `User.table.ts`) |
---
+++ generator.ts
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-case-declarations */
+import path from 'path'
+import fs from 'fs-extra'
+import knex from 'knex'
+import clear from 'clear-any-console'
+clear()
+
+function getType(descType: Desc['Type']) {
+  const type = descType.split('(')[0]
+  switch (type) {
+    case 'date':
+    case 'datetime':
+    case 'timestamp':
+    case 'time':
+    case 'year':
+    case 'char':
+    case 'varchar':
+    case 'tinytext':
+    case 'text':
+    case 'mediumtext':
+    case 'longtext':
+    case 'json':
+    case 'decimal':
+      return 'Type.String()'
+    case 'tinyint':
+    case 'smallint':
+    case 'mediumint':
+    case 'int':
+    case 'bigint':
+    case 'float':
+    case 'double':
+      return 'Type.Number()'
+    case 'enum':
+      const value = descType.replace('enum(', '').replace(')', '').replaceAll(',', ', ')
+      return `Type.Unsafe<${value.replaceAll(', ', ' | ')}>({ type: 'string', enum: [${value}] })`
+  }
+}
+
+const config = fs.readJSONSync(path.join(process.cwd(), 'mysql-typebox.json')) as Config
+if (config.folder && config.folder !== '')
+  fs.emptyDirSync(config.folder)
+
+async function generate(config: Config) {
+  const db = knex({
+    client: 'mysql2',
+    connection: {
+      host: config.host,
+      port: config.port,
+      user: config.user,
+      password: config.password,
+      database: config.database,
+    },
+  })
+
+  const t = await db.raw('SELECT table_name FROM information_schema.tables WHERE table_schema = ?', [config.database])
+  let tables = t[0].map((row: any) => row.table_name).filter((table: string) => !table.startsWith('knex_')).sort() as Tables
+
+  if (config.tables && config.tables.length)
+    tables = tables.filter(table => config.tables.includes(table))
+
+  if (config.ignore && config.ignore.length)
+    tables = tables.filter(table => !config.ignore.includes(table))
+
+  for (const table of tables) {
+    const d = await db.raw(`DESC ${table}`)
+    const describes = d[0] as Desc[]
+    let content = `import { Type } from '@sinclair/typebox'
+
+export const ${table} = Type.Object({`
+    for (const desc of describes) {
+      const field = desc.Field
+      const type = getType(desc.Type)
+      const isNull = desc.Null === 'YES'
+      content = `${content}
+  ${field}: ${isNull ? `Type.Optional(Type.Union([${type}, Type.Null()]))` : `${type}`},`
+    }
+    content = `${content}
+})
+`
+    const dir = config.folder && config.folder !== '' ? config.folder : '.'
+    const file = config.suffix && config.suffix !== '' ? `${table}.${config.suffix}.ts` : `${table}.ts`
+    const dest = path.join(dir, file)
+    console.log('Created:', dest)
+    fs.outputFileSync(dest, content)
+  }
+  process.exit(1)
+}
+
+(async () => {
+  await generate(config)
+})()
+
+type Tables = string[]
+interface Desc {
+  Field: string
+  Type: string
+  Null: 'YES' | 'NO'
+}
+interface Config {
+  host: string
+  port: number
+  user: string
+  password: string
+  database: string
+  tables?: string[]
+  ignore?: string[]
+  folder?: string
+  suffix?: string
+}
